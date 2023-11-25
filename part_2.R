3118fef612a010bc25ed2c92a3d18adce9e719d3
NadjaLa <150966651+NadjaLa@users.noreply.github.com>
2023-11-24 18:17:26 -0600

    Update part_2.R
    basic stuff, classifying based on matched/mismatch and gap scores bc I'm still figuring out how to implement checking for the specific sequences averi sent. will need to be changed

--- part_2.R
+++ part_2.R
@@ -1,1 +1,26 @@
 # naive bayes or hhm
+# this is not by any means finidhed, feel free to totally overwrite this. was just putting a general idea
+
+library(e1071)
+
+classify_sequence <- function(seq1, seq2, match_score, mismatch_score, gap_penalty) {
+  alignment_result <- global_alignment(seq1, seq2, match_score, mismatch_score, gap_penalty)
+  
+  matched_residues <- sum(strsplit(alignment_result$align_seq1, "")[[1]] == strsplit(alignment_result$align_seq2, "")[[1]])
+  mismatched_residues <- alignment_result$alignment_score - matched_residues
+  gaps <- sum(strsplit(alignment_result$align_seq1, "")[[1]] == "-" | strsplit(alignment_result$align_seq2, "")[[1]] == "-")
+  
+  data <- data.frame(
+    Matched = matched_residues,
+    Mismatched = mismatched_residues,
+    Gaps = gaps,
+    Class = c("bird", "mammalian")
+  )
+  
+  nb_classifier <- naiveBayes(Class ~ ., data = data)
+  
+  return(nb_classifier)
+}
+
+# new_data <- data.frame(MatchedResidues = x, MismatchedResidues = x, Gaps = x)
+# predicted_class <- predict(classifier, new_data)
